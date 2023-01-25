1997b91db3d0d621b6a5a136d4167499be2bcec5
Natan Felles <natanfelles@gmail.com>
2023-01-25 18:30:23 -0300

    feat: prepare database to test CustomerController

--- tests/app/Controllers/CustomerControllerTest.php
+++ tests/app/Controllers/CustomerControllerTest.php
@@ -9,6 +9,8 @@
  */
 namespace Tests\app\Controllers;
 
+use App;
+use Framework\Database\Definition\Table\TableDefinition;
 use Framework\HTTP\Status;
 use Tests\TestCase;
 
@@ -17,6 +19,33 @@ use Tests\TestCase;
  */
 class CustomerControllerTest extends TestCase
 {
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->prepareDatabase();
+    }
+
+    protected function prepareDatabase() : void
+    {
+        $database = App::database('default');
+        $table = 'customers';
+        $database->dropTable($table)->ifExists()->run();
+        $database->createTable($table)
+            ->definition(static function (TableDefinition $def) : void {
+                $def->column('id')->int(11)->primaryKey()->autoIncrement();
+                $def->column('name')->varchar(32);
+                $def->column('birthday')->date();
+                $def->column('gender')->enum('male', 'female');
+                $def->column('healthProblem')->text();
+            })->run();
+        $database->insert($table)
+            ->columns('name', 'birthday', 'gender', 'healthProblem')
+            ->values([
+                ['John Doe', '2000-10-31', 'male', 'gripe'],
+                ['Mary Doe', '2002-05-20', 'female', 'covid'],
+            ])->run();
+    }
+
     public function testIndex() : void
     {
         //$this->app->runHttp('http://localhost:8080/customers');
