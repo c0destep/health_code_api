9199eff36c07110d6eb488c1894a7dfe29826174
Natan Felles <natanfelles@gmail.com>
2023-01-25 18:31:59 -0300

    fix: make CustomerControllerTests works

--- tests/app/Controllers/CustomerControllerTest.php
+++ tests/app/Controllers/CustomerControllerTest.php
@@ -48,49 +48,43 @@ class CustomerControllerTest extends TestCase
 
     public function testIndex() : void
     {
-        //$this->app->runHttp('http://localhost:8080/customers');
-        //self::assertResponseStatusCode(Status::OK);
-        //self::assertMatchedRouteName('customers.index');
-        self::assertSame('index', 'index');
+        $this->app->runHttp('http://localhost:8080/customers');
+        self::assertResponseStatusCode(Status::OK);
+        self::assertMatchedRouteName('customers.index');
     }
 
     public function testShow() : void
     {
-        //$this->app->runHttp('http://localhost:8080/customers/1');
-        //self::assertResponseStatusCode(Status::OK);
-        //self::assertMatchedRouteName('customers.show');
-        self::assertSame('show', 'show');
+        $this->app->runHttp('http://localhost:8080/customers/1');
+        self::assertResponseStatusCode(Status::OK);
+        self::assertMatchedRouteName('customers.show');
     }
 
     public function testCreate() : void
     {
-        //$this->app->runHttp('http://localhost:8080/customers', 'POST');
-        //self::assertResponseStatusCode(Status::OK);
-        //self::assertMatchedRouteName('customers.create');
-        self::assertSame('create', 'create');
+        $this->app->runHttp('http://localhost:8080/customers', 'POST');
+        self::assertResponseStatusCode(Status::OK);
+        self::assertMatchedRouteName('customers.create');
     }
 
     public function testUpdate() : void
     {
-        //$this->app->runHttp('http://localhost:8080/customers/1', 'PATCH');
-        //self::assertResponseStatusCode(Status::OK);
-        //self::assertMatchedRouteName('customers.update');
-        self::assertSame('update', 'update');
+        $this->app->runHttp('http://localhost:8080/customers/1', 'PATCH');
+        self::assertResponseStatusCode(Status::OK);
+        self::assertMatchedRouteName('customers.update');
     }
 
     public function testReplace() : void
     {
-        //$this->app->runHttp('http://localhost:8080/customers/1', 'PUT');
-        //self::assertResponseStatusCode(Status::OK);
-        //self::assertMatchedRouteName('customers.replace');
-        self::assertSame('replace', 'replace');
+        $this->app->runHttp('http://localhost:8080/customers/1', 'PUT');
+        self::assertResponseStatusCode(Status::OK);
+        self::assertMatchedRouteName('customers.replace');
     }
 
     public function testDelete() : void
     {
-        //$this->app->runHttp('http://localhost:8080/customers/1', 'DELETE');
-        //self::assertResponseStatusCode(Status::OK);
-        //self::assertMatchedRouteName('customers.delete');
-        self::assertSame('delete', 'delete');
+        $this->app->runHttp('http://localhost:8080/customers/1', 'DELETE');
+        self::assertResponseStatusCode(Status::OK);
+        self::assertMatchedRouteName('customers.delete');
     }
 }
